258761650ad01a1957a9a36cbb9fb5db2a6af5d1
haomingxuan <haomingxuan@bytedance.com>
2021-08-14 13:54:20 +0800

    feat:some

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,32 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 import { View, Text } from '@tarojs/components'
-import { AtButton } from 'taro-ui'
-
-import "taro-ui/dist/style/components/button.scss" // 按需引入
+import { AtForm,AtInput } from 'taro-ui'
 import './index.less'
 
-export default class Index extends Component {
-
-  componentWillMount () { }
-
-  componentDidMount () { }
-
-  componentWillUnmount () { }
-
-  componentDidShow () { }
-
-  componentDidHide () { }
-
-  render () {
+const Index = () => {
+    const [ company,setCompany ] = useState('')
+    console.log(company);
     return (
       <View className='index'>
-        <Text>Hello world!</Text>
-        <AtButton type='primary'>I need Taro UI</AtButton>
-        <Text>Taro UI 支持 Vue 了吗？</Text>
-        <AtButton type='primary' circle={true}>支持</AtButton>
-        <Text>共建？</Text>
-        <AtButton type='secondary' circle={true}>来</AtButton>
+        <View className='search-panel'>
+          <AtForm>
+            <AtInput value={company} onChange={value => setCompany(value as string)} name='company' title='公司' type='text' placeholder='输入关键词'></AtInput>
+          </AtForm>
+        </View>
       </View>
     )
-  }
 }
+
+export default Index
